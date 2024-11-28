daa924e06f0a0d42914602a389c27e36566fd2ed
Anderson V Santos <andersonvs.ju@gmail.com>
2024-11-28 09:12:40 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int numero = 10;
+        System.out.println(numero);
+        int numero1 = Integer.valueOf(numero);
+        System.out.println(numero1);
+    }
+}
